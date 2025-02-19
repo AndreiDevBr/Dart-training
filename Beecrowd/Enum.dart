9db5b6466b637ab282d3c8bfe6079962b3e3430d
oldapps <old.apps2024@gmail.com>
2025-02-19 01:23:53 -0300

    Novo commit

---
+++ Beecrowd/Enum.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+enum UserType { admin, user, costumer }
+
+void main() {
+  final userType = UserType.admin;
+  print(userType);
+
+  switch (userType) {
+    case UserType.admin:
+      {
+        print("Ele é adminstrador");
+      }
+      break;
+    default:
+      "NADA";
+    
+  }
+  
+}
